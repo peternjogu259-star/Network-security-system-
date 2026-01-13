0e0b745fd8178d82127e9e932ab4e00ceab6c6b1
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-13 12:00:00 +0000

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,31 @@
+# configuration info about the project
+from setuptools import find_packages,setup
+from typing import List
+
+# function to return list of requirements
+def get_requirements()-> List[str]:
+    requirement_lst:List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_lst
+
+print(get_requirements())
+
+# Setting up meta data
+setup(
+    name = "Network Security System",
+    version = "0.0.1",
+    author = "Jimmy",
+    author_email = "jimmymuthoni26@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
