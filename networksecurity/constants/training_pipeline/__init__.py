2b14a6b9d617821150f44f46fcbb5ad395306b6a
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-01-13 12:00:00 +0000

    Data Trasformation Commit

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 # defining common constant variable for training pipeline
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
@@ -25,3 +25,16 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+
+# Data transfromation constants
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+# kkn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
